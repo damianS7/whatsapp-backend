8577b156718acd91d69c3503fe3ba4440726ea9f
Damian <josepwnz@gmail.com>
2025-06-14 20:26:44 +0200

    added Room

--- src/main/java/com/damian/words/chat/room/RoomService.java
+++ src/main/java/com/damian/words/chat/room/RoomService.java
@@ -1,15 +1,41 @@
 package com.damian.words.chat.room;
 
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class RoomService {
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final RoomRepository roomRepository;
 
     public RoomService(
-            BCryptPasswordEncoder bCryptPasswordEncoder
+            RoomRepository roomRepository
     ) {
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.roomRepository = roomRepository;
+    }
+
+    public List<Room> getRooms() {
+        return roomRepository.findAll();
+    }
+
+    public Room getRoom(Long id) {
+        return roomRepository.findById(id).orElse(null);
+    }
+
+    public Room createRoom(Room room) {
+        return roomRepository.save(room);
+    }
+
+    public void joinRoom(Long id) {
+        // check if banned
+        // check room exists
+        // check if user is in room
+    }
+
+    public String messageToRoom(Long id, String message) {
+        // check if banned
+        // check room exists
+        // check if user is in room
+        return "";
     }
 }
