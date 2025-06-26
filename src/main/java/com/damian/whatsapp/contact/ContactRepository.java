0ce354403d9bdededf6d062425605e02731c3f16
Damian <josepwnz@gmail.com>
2025-06-27 01:13:05 +0200

    added check for existing contacts before adding.

--- src/main/java/com/damian/whatsapp/contact/ContactRepository.java
+++ src/main/java/com/damian/whatsapp/contact/ContactRepository.java
@@ -1,6 +1,8 @@
 package com.damian.whatsapp.contact;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Set;
@@ -8,5 +10,8 @@ import java.util.Set;
 @Repository
 public interface ContactRepository extends JpaRepository<Contact, Long> {
     Set<Contact> findAllByCustomerId(Long customerId);
+
+    @Query("SELECT COUNT(c) > 0 FROM Contact c WHERE c.customer.id = :customerId AND c.contact.id = :contactCustomerId")
+    boolean contactExists(@Param("customerId") Long customerId, @Param("contactCustomerId") Long contactCustomerId);
 }
 
--- src/main/java/com/damian/whatsapp/contact/ContactService.java
+++ src/main/java/com/damian/whatsapp/contact/ContactService.java
@@ -2,7 +2,9 @@ package com.damian.whatsapp.contact;
 
 import com.damian.whatsapp.common.exception.Exceptions;
 import com.damian.whatsapp.common.utils.AuthHelper;
+import com.damian.whatsapp.contact.exception.ContactAlreadyExistException;
 import com.damian.whatsapp.contact.exception.ContactAuthorizationException;
+import com.damian.whatsapp.contact.exception.ContactNotFoundException;
 import com.damian.whatsapp.contact.exception.MaxContactsLimitReachedException;
 import com.damian.whatsapp.customer.Customer;
 import com.damian.whatsapp.customer.CustomerRepository;
@@ -31,7 +33,7 @@ public class ContactService {
         return contactRepository.findAllByCustomerId(loggedCustomer.getId());
     }
 
-    // add a new friend for the logged customer
+    // add a new contact for the logged customer
     public Contact addContact(Long customerId) {
         Customer loggedCustomer = AuthHelper.getLoggedCustomer();
 
@@ -40,26 +42,31 @@ public class ContactService {
             throw new MaxContactsLimitReachedException(Exceptions.CONTACT_LIST.MAX_CONTACTS);
         }
 
-        // check if the customer we are trying to add as a contact exists
-        Customer contact = customerRepository.findById(customerId).orElseThrow(
+        // check if the customer we want to add as a contact exists.
+        Customer contactCustomer = customerRepository.findById(customerId).orElseThrow(
                 () -> new CustomerNotFoundException(Exceptions.CUSTOMER.NOT_FOUND)
         );
 
+        // check if that they are not already contact
+        if (contactRepository.contactExists(loggedCustomer.getId(), contactCustomer.getId())) {
+            throw new ContactAlreadyExistException(Exceptions.CONTACT_LIST.ALREADY_EXISTS);
+        }
+
         return contactRepository.save(
-                new Contact(loggedCustomer, contact)
+                new Contact(loggedCustomer, contactCustomer)
         );
     }
 
-    // delete a contact from the friendlist of the logged customer
+    // delete a contact from the contact list of the logged customer.
     public void deleteContact(Long id) {
         Customer loggedCustomer = AuthHelper.getLoggedCustomer();
 
-        // check if the friendship exists
+        // check if the contact exists
         Contact contact = contactRepository.findById(id).orElseThrow(
-                () -> new CustomerNotFoundException(Exceptions.CUSTOMER.NOT_FOUND)
+                () -> new ContactNotFoundException(Exceptions.CONTACT_LIST.NOT_FOUND)
         );
 
-        // check if the logged customer is the owner of the friendship
+        // check if the logged customer is the owner of the contact.
         if (!loggedCustomer.getId().equals(contact.getCustomer().getId())) {
             throw new ContactAuthorizationException(Exceptions.CONTACT_LIST.ACCESS_FORBIDDEN);
         }
