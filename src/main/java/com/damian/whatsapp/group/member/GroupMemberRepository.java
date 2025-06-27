c0a8318a731c5b0c1cf7d7c3de840f0af405b592
Damian <josepwnz@gmail.com>
2025-06-28 00:59:49 +0200

    add method to find all members from groupId

--- src/main/java/com/damian/whatsapp/group/member/GroupMemberRepository.java
+++ src/main/java/com/damian/whatsapp/group/member/GroupMemberRepository.java
@@ -3,7 +3,10 @@ package com.damian.whatsapp.group.member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface GroupMemberRepository extends JpaRepository<GroupMember, Long> {
+    Set<GroupMember> findByGroupId(Long groupId);
 }
 
