072ada2dabb9675eb116faa516e9ee0861183683
Damian <josepwnz@gmail.com>
2025-09-28 01:58:41 +0200

    added parent exception for Group

--- src/main/java/com/damian/whatsapp/modules/group/exception/GroupAuthorizationException.java
+++ src/main/java/com/damian/whatsapp/modules/group/exception/GroupAuthorizationException.java
@@ -1,10 +1,8 @@
 package com.damian.whatsapp.modules.group.exception;
 
 
-import com.damian.whatsapp.shared.exception.ApplicationException;
-
-public class GroupAuthorizationException extends ApplicationException {
-    public GroupAuthorizationException(String message) {
-        super(message);
+public class GroupAuthorizationException extends GroupException {
+    public GroupAuthorizationException(String message, Long groupId) {
+        super(message, groupId);
     }
 }
